46d125c78194bb9524fe6186142f09da52d5ac82
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-10-25 23:28:58 -0500

    added data

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var endpoint = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var computation = require('./routes/users')
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/mydata', endpoint);
 
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
