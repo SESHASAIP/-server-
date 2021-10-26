8724f722871b1ef0e0262c0ae19dabc1cf2df1d8
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-10-25 23:22:03 -0500

    added bonus

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var value= 0;
-var rand=Math.floor(Math.random() * 10);
+var rand=Math.floor(Math.random());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   value=1;
-  res.send('User accesses are:'+ value+'jai balayya');
-  res.send("jai balayya");
+  res.send('User accesses are:'+ value+'\n'+'[Math.log()]'+ 'applied to ['+rand+'] is ['+Math.log(rand)+']');
+  
   
 });
 
