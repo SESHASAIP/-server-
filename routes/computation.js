e884b99db11704221f23617b66af3f13132afb03
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-10-27 10:27:40 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var vale=Math.random();
-var res=Math.log(vale);
+var r=Math.log(vale);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-  res.send('the function[Math.log() applied to ] ['+ vale+'] is ['+res+']');
+  res.send('the function[Math.log() applied to ] ['+ vale+'] is ['+r+']');
   
   
 });
