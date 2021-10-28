c2251ae1cb91ee63741f95565757239739981919
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-10-27 23:34:53 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -6,7 +6,9 @@ var r=Math.log(vale);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-  res.send('the function[Math.log() applied to ] ['+ vale+'] is ['+r+']');
+  res.send('the function[Math.log() applied to ] ['+ vale+'] is ['+r+']\n'+
+  'the function[ Math.log10()  applied to ] ['+ vale+'] is ['+ Math.log10(vale) +']\n'+
+  'the function[ Math.imul()  applied to ] ['+ vale+'] is ['+ Math.imul(vale) +']\n' );
   
   
 });
