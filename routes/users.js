94b0620dcbb6ea692729325813063fe92a4692a3
SESHASAIP <89412075+SESHASAIP@users.noreply.github.com>
2021-10-29 00:00:21 -0500

    added code

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var value= 0;
-var rand=Math.floor(Math.random());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  value=1;
-  res.send('User accesses are:'+ value+'\n'+'[Math.log()]'+ 'applied to ['+rand+'] is ['+Math.log(rand)+']');
+  value=value+1;
+  res.send('User accesses are:'+ value);
   
   
 });
